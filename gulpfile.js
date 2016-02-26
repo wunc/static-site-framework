71c7762579b3fea366dff8a1ce3633029c59c6b4
Wun Chiou <wun@utdallas.edu>
2016-02-25 23:57:37 -0600

    finish creating gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@
 var paths = {
 	templates: './templates',
 	sass: 'css/source',
-	css: 'css'
+	css: 'css',
+	js: 'js',
+	bootstrapjs: 'vendor/bower_components/bootstrap-sass/assets/javascripts',
+	jquery: 'vendor/bower_components/jquery/dist'
 };
 
 var gulp = require('gulp');
@@ -12,7 +15,7 @@ var fileinclude = require('gulp-file-include');
 var sass = require('gulp-ruby-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('fileinclude', function() {
+gulp.task('html', function() {
 	return gulp.src(paths.templates + '/*.tpl.html')
 		.pipe(fileinclude())
 		.pipe(rename({
@@ -24,7 +27,7 @@ gulp.task('fileinclude', function() {
 		.pipe(gulp.dest('./'));
 });
 
-gulp.task('sass', function() {
+gulp.task('css', function() {
 	return sass(paths.sass + '/style.scss', {
 			style: 'compressed',
 			sourcemap: true,
@@ -36,3 +39,25 @@ gulp.task('sass', function() {
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(paths.css));
 });
+
+gulp.task('copy_bootstrapjs', function() {
+	return gulp.src(paths.bootstrapjs + '/bootstrap.min.js')
+		.pipe(gulp.dest(paths.js));
+});
+
+gulp.task('copy_jquery', function() {
+	return gulp.src(paths.jquery + '/jquery.min.js')
+		.pipe(gulp.dest(paths.js));
+});
+
+// Copy stuff
+gulp.task('copy', ['copy_bootstrapjs', 'copy_jquery']);
+
+// Watch
+gulp.task('watch', function() {
+     gulp.watch(paths.sass + '/**/*.scss', ['css']); 
+	gulp.watch(paths.templates + '/**/*.html', ['html']); 
+});
+
+// Default
+  gulp.task('default', ['html', 'css']);
