319a204e55143be46e529062f30549a650532efa
Wun Chiou <wun@utdallas.edu>
2016-02-25 22:38:26 -0600

    Implement gulp-file-include

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
 /** Gulp file for tasks */
 
+var paths = {
+	templates: './templates',
+	sass: 'css/source'
+};
+
 var gulp = require('gulp');
+var path = require('path');
 var rename = require('gulp-rename');
+var fileinclude = require('gulp-file-include');
 var sass = require('gulp-ruby-sass');
 var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('fileinclude', function() {
+	return gulp.src(path.join(paths.templates, '*.tpl.html'))
+		.pipe(fileinclude())
+		.pipe(rename({
+			extname: ""
+		}))
+		.pipe(rename({
+			extname: ".html"
+		}))
+		.pipe(gulp.dest('./'));
+});
