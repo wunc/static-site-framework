4f9cfbc26ed5b926d30a4daa86bab869d9ab5b69
Wun Chiou <wun@utdallas.edu>
2016-02-25 23:17:01 -0600

    Implement sass compiling

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,18 @@
 
 var paths = {
 	templates: './templates',
-	sass: 'css/source'
+	sass: 'css/source',
+	css: 'css'
 };
 
 var gulp = require('gulp');
-var path = require('path');
 var rename = require('gulp-rename');
 var fileinclude = require('gulp-file-include');
 var sass = require('gulp-ruby-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('fileinclude', function() {
-	return gulp.src(path.join(paths.templates, '*.tpl.html'))
+	return gulp.src(paths.templates + '/*.tpl.html')
 		.pipe(fileinclude())
 		.pipe(rename({
 			extname: ""
@@ -22,4 +22,17 @@ gulp.task('fileinclude', function() {
 			extname: ".html"
 		}))
 		.pipe(gulp.dest('./'));
+});
+
+gulp.task('sass', function() {
+	return sass(paths.sass + '/style.scss', {
+			style: 'compressed',
+			sourcemap: true,
+			cacheLocation: '/tmp/sass-cache'
+		})
+		.on('error', function (err) {
+			console.error('Error!', err.message);
+		})
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(paths.css));
 });
